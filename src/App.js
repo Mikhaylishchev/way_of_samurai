85dabb42746818541270f7f92e02609a3bb67159
Evgeny_Mikhaylishchev <shady942@gmail.com>
2022-02-12 13:53:03 +0500

    markup

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
 import './App.css';
@@ -5,39 +6,50 @@ import './App.css';
 function App() {
   return (
 
-      <div className="App">
+      <div className="app-wrapper">
 
-        <Header />
+        <header className="header">
+          <img src="https://clck.ru/avtRN"></img>
+        </header>
 
-        <Technologies/>
+        <nav className="nav">
+          <div>
+            <a>Profile</a>
+          </div>
+          <div>
+            <a>Messages</a>
+          </div>
+          <div>
+            <a>News</a>
+          </div>
+          <div>
+            <a>Music</a>
+          </div>
+          <div>
+            <a>Settings</a>
+          </div>
+        </nav>
+
+        <div className="content">
+          <img src="https://clck.ru/b4sKL"></img>
+          <div>
+            avatar + info
+          </div>
+          <div>
+            posts
+            <div>
+              new post
+            </div>
+            <div>post-1</div>
+            <div>post-2</div>
+            <div>post-3</div>
+          </div>
+        </div>
+        
+        
         
       </div>
   );
-}
-
-const Technologies = () => {
-  
-  return (
-
-    <ul>
-      <li>HTML</li>
-      <li>CSS</li>
-      <li>JAVASCRIPT</li>
-      <li>REACT</li>
-    </ul>
-  )
-}
-
-const Header = () => {
-
-  return (
-
-    <div>
-      <a href="#">Home</a>
-      <a href="#">News</a>
-      <a href="#">Messages</a>
-    </div>
-  )
 }
 
 export default App;
