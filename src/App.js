92be0c54342b4089c643b3921a557181b767bec6
Evgeny_Mikhaylishchev <shady942@gmail.com>
2022-02-13 22:44:35 +0500

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -5,19 +5,35 @@ import './App.css';
 import Profile from './Components/Profile/Profile';
 import Header from './Components/Header/Header';
 import Menu from './Components/Menu/Menu';
+import Dialogs from './Components/Dialogs/Dialogs';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
-
+    <BrowserRouter>
       <div className="app-wrapper">
 
         <Header />
 
         <Menu />
 
-        <Profile />
+        <div className="app-wrapper-content">
+
+        <Routes>
+
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/messages" element={<Dialogs />} />
+          {/* <Route path="/news" element={<News />} /> 
+          <Route path="/music" element={<Music />} /> 
+          <Route path="/settings" element={<Settings />} /> */}
+
+        </Routes>
+
+        </div>
   
       </div>
+      </BrowserRouter>
   );
 }
 
---
+++ src/Components/Dialogs/Dialogs.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react';
+// import s from './Dialogs.module.css'
+
+const Dialogs = (props) => {
+
+  return(
+
+    <div>
+      Dialogs
+    </div>
+  )
+}
+
+export default Dialogs;
