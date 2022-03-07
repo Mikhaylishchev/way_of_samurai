01291cee23a668b5fe6982ce987807092413cc75
Evgeny_Mikhaylishchev <shady942@gmail.com>
2022-03-07 08:40:30 +0500

    side-effect

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -4,47 +4,51 @@ import * as axios from 'axios';
 
 const Users = (props) => {
 
-    if(props.users.length === 0) {
+    let getUsers = () => {
 
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
-        
-        .then(response => {
-            
-            props.setUsers(response.data.items)
-        })
+        if (props.users.length === 0) {
 
-        /* props.setUsers( [
-        
-            {id: 1, followed: false, avavtarUrl: 'https://clck.ru/bVQVw', firstName: 'Jack', lastName: 'Shepard', status: 'Lorem ipsum dolor sit amet.', location: {country: 'USA', state: 'California', city: 'Los Angeles'}},
-            {id: 2, followed: true, avavtarUrl: 'https://clck.ru/dW87U', firstName: 'John', lastName: 'Locke', status: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', location: {country: 'USA', state: 'California', city: 'Los Angeles'}},
-            {id: 3, followed: true, avavtarUrl: 'https://clck.ru/bYqmB', firstName: 'James', lastName: 'Ford', status: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, aliquid!', location: {country: 'USA', state: 'California', city: 'Los Angeles'}},
-        ]) */
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
 
+                .then(response => {
+
+                    props.setUsers(response.data.items)
+                })
+        }
     }
 
-    return props.users.map(user => <div key={`${user.id}/* ${user.name} */`}>
-        
-        <div className={s.friendsWrapper}>
-            <span>
+    return <div>
 
-                <div>
-                    <img className={s.avatar} src={user.photos.small || 'https://clck.ru/b2h9v'} alt='avatar'></img>
+        <button onClick={getUsers}>Get Users</button>
+
+        {
+
+            props.users.map(user => <div key={`${user.id}/* ${user.name} */`}>
+
+                <div className={s.friendsWrapper}>
+                    <span>
+
+                        <div>
+                            <img className={s.avatar} src={user.photos.small || 'https://clck.ru/b2h9v'} alt='avatar'></img>
+                        </div>
+
+                        <div>
+                            {<button onClick={() => { props.following(user.id) }}>{user.followed ? 'Unfollow' : 'Follow'}</button>}
+                        </div>
+                    </span>
+
+                    <span className={s.information}>
+                        <div>{`${user.name}`}</div>
+                        <div>{'country'}, {'state'}, {'city'}</div>
+                        <div>{user.status}</div>
+                    </span>
+
                 </div>
+            </div>)
 
-                <div>
-                    {<button onClick={() => {props.following(user.id)}}>{user.followed ? 'Unfollow' : 'Follow'}</button>}
-                </div>
-            </span>    
+        }
         
-            <span className={s.information}>
-                <div>{`${user.name}`}</div>
-                <div>{'country'}, {'state'}, {'city'}</div>
-                <div>{user.status}</div>
-            </span>
-
-            </div>
-    </div>)
-
+    </div>
 }
 
 export default Users;
